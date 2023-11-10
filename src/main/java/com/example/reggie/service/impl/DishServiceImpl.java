d10f7facde01547762aa2e9e9b9bedc94eb62827
“Kawhi-95” <420021407@qq.com>
2023-11-10 08:33:12 +0800

    DIsh相关

---
+++ src/main/java/com/example/reggie/service/impl/DishServiceImpl.java
@@ -0,0 +1,11 @@
+package com.example.reggie.service.impl;
+
+import com.baomidou.mybatisplus.extension.service.impl.ServiceImpl;
+import com.example.reggie.entity.Dish;
+import com.example.reggie.mapper.DishMapper;
+import com.example.reggie.service.DishService;
+import org.springframework.stereotype.Service;
+
+@Service
+public class DishServiceImpl extends ServiceImpl<DishMapper, Dish> implements DishService {
+}
